ca6baef0f28cf3bf011234fc3d7f140c6ef047c9
eden <email4edenh@gmail.comgit config user.name>
2025-02-13 20:10:10 -0600

    1

--- SpeedDetector.java
+++ SpeedDetector.java
@@ -1,5 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class SpeedDetector {
     public static void main(String[] args) {
-        
+
+        String input;
+        input = JOptionPane.showInputDialog("Please enter your speed");
+        int speed = Integer.parseInt(input);
+        JOptionPane.showMessageDialog(null, "Your speed is " + speed);
+
+        if (speed > 60) {
+            JOptionPane.showMessageDialog(null, "You are speeding", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        System.exit(0);
     }
 }
