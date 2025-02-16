a8f2bc74a9caaba14d5637d7352407d0db30b720
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 14:31:05 -0600

    1

---
+++ MultiplesOfTen.java
@@ -0,0 +1,15 @@
+public class MultiplesOfTen {
+    public static void main(String[] args) {
+        int count = 0;
+
+         for (int number = 10; number <= 1000; number += 10) {
+            System.out.println(+ number);
+            count++;
+
+            if (count % 10 == 0)    {
+                System.out.println();
+            }
+         }
+    }
+    
+}
