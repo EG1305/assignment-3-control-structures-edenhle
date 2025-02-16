ebf8397bc709472bf4eed64e217f00664a9d015a
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 16:02:18 -0600

    sum

--- Summation.java
+++ Summation.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Summation {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number;
+
+        System.out.println("This program will add all the numbers you enter");
+        System.out.println("Enter 0 for me to stop adding");
+
+        while (true)    {
+            number = scanner.nextInt();
+
+            System.out.println("You entered " + number);
+
+            if (number == 0)    {
+                break;
+            }
+            sum += number;
+            System.out.println("The sum is " + sum);
+        }
+        System.out.println("The sum of all numbers you entered is " + sum);
+
+        scanner.close();
     }
-    
 }
+    
+ 
