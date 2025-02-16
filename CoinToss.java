2cc586d41e3deb6b221f892a12e0fbad086e853b
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 16:43:19 -0600

    yes

---
+++ CoinToss.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CoinToss {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many coin tosses do you need?");
+        int number = scanner.nextInt();
+        
+        System.out.println("Now I will toss the coin " + number + " times.");
+
+        int heads = 0;
+        int tails = 0;
+
+        for(int i = 0; i < number; i++) {
+            String result = (Math.random() < 0.5) ? "tails" : "heads";
+            System.out.println("Toss: " + result);
+
+            if (result.equals("heads")) {
+                heads++;
+            } else  {
+                tails++;
+            }
+        }
+        System.out.println("Heads facing up: " + heads);
+        System.out.println("Tails facing up: " + tails);
+    }
+}
