6529e59e28ba0e2dc2251cb00523dc86a3e1629e
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 16:15:05 -0600

    12

--- PINCheck.java
+++ PINCheck.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class PINCheck {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your PIN");
+        String pin = scanner.nextLine();
+        String enteredPin;
+
+        do{
+            System.out.println("Please confirm your PIN");
+            enteredPin = scanner.nextLine();
+            if  (!pin.equals(enteredPin))   {
+                System.out.println("Incorrect PIN. Try again");
+            }
+        }   while   (!pin.equals(enteredPin));
+
+        System.out.println("PIN accepted. You can access your account");
+        scanner.close();
     }
 }
