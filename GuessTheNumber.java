156fd7e12e4ad7703bf88205f82af60ca4b0409c
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 17:13:32 -0600

    12

---
+++ GuessTheNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int random = (int) (Math.random() * 101);
+        int guess;
+        boolean guessed = false;
+
+        System.out.println("Guess a number between 0 and 100");
+
+        while (!guessed) {
+            System.out.println("Enter your guess: " );
+            guess = scanner.nextInt();
+
+            if (guess == random) {
+                System.out.println("Yes, the number is " + guess);
+                guessed = true;
+            } else if (guess < random)  {
+                System.out.println("Your guess is too low");
+            } else  {
+                System.out.println("Your guess is too high");
+            } 
+        }
+        scanner.close();
+    }
+}
--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -7,7 +7,6 @@ public class SumOfNumbers {
         System.out.println("This program will add all the numbers from 1 to the number you enter");
         System.out.println("Please enter a number");
         int number = scanner.nextInt();
-
         int sum = 0;
         
         for (int i = 1; i <= number; i++)   {
