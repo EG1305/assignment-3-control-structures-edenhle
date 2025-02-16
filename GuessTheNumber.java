01d5b7d71e804518f2779364dedb6b7e234b833b
eden <email4edenh@gmail.comgit config user.name>
2025-02-16 17:31:36 -0600

    13

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -5,6 +5,7 @@ public class GuessTheNumber {
         Scanner scanner = new Scanner(System.in);
         int random = (int) (Math.random() * 101);
         int guess;
+        int guesses = 0;
         boolean guessed = false;
 
         System.out.println("Guess a number between 0 and 100");
@@ -12,16 +13,20 @@ public class GuessTheNumber {
         while (!guessed) {
             System.out.println("Enter your guess: " );
             guess = scanner.nextInt();
+            guesses++;
 
             if (guess == random) {
                 System.out.println("Yes, the number is " + guess);
                 guessed = true;
+
+                System.out.println("You guessed the correct number in " + guesses + " guesses");
+
             } else if (guess < random)  {
                 System.out.println("Your guess is too low");
             } else  {
                 System.out.println("Your guess is too high");
-            } 
+                }  
+            }
+            scanner.close();
         }
-        scanner.close();
     }
-}
