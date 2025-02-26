e862644f0353fa56dc17677ae1e655bed67a090d
eden <email4edenh@gmail.comgit config user.name>
2025-02-25 22:12:51 -0600

    122

--- CoinToss.java
+++ CoinToss.java
@@ -4,13 +4,15 @@ public class CoinToss {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //user input amount of coin tosses
         System.out.println("How many coin tosses do you need?");
         int number = scanner.nextInt();
         
+        //print amount of times coin will be tossed
         System.out.println("Now I will toss the coin " + number + " times.");
 
-        int heads = 0;
-        int tails = 0;
+        int heads = 0;  //counter for heads
+        int tails = 0;  //counter for tails
 
         for(int i = 0; i < number; i++) {
             String result = (Math.random() < 0.5) ? "tails" : "heads";
@@ -22,8 +24,8 @@ public class CoinToss {
                 tails++;
             }
         }
-        System.out.println("Heads facing up: " + heads);
-        System.out.println("Tails facing up: " + tails);
+        System.out.println("Heads facing up: " + heads); //print amount of times head is facing up
+        System.out.println("Tails facing up: " + tails); //print amount of times tails is facing up
 
         scanner.close();
     }
--- PINCheck.java
+++ PINCheck.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class PINCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        //user input PIN
         System.out.println("please enter your PIN");
         String pin = scanner.nextLine();
         String enteredPin;
 
         do{
+            //re enter PIN
             System.out.println("Please confirm your PIN");
             enteredPin = scanner.nextLine();
+
+            //if confirmed PIN is not equal to PIN, try again
             if  (!pin.equals(enteredPin))   {
                 System.out.println("Incorrect PIN. Try again");
             }
