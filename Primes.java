a3b62f90d0d45a84eab0f4e52d7da0e9a365ddb4
eden <email4edenh@gmail.comgit config user.name>
2025-02-25 22:23:40 -0600

    12

--- Primes.java
+++ Primes.java
@@ -23,12 +23,12 @@ public class Primes {
             //if conditions for prime number is met
             if (isPrime)    {
                 //prints prime numbers below user input
-                System.out.println(i);
+                System.out.print(i + "  ");
                 count++;    //counts prime numbers
             }
         }
         //prints amount of prime numbers
-        System.out.println("There are a total of " + count + " prime numbers.");
+        System.out.println("\nThere are a total of " + count + " prime numbers.");
         scanner.close();
     }
 }
